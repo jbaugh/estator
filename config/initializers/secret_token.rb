436f3e7812c51b54839a88a098f5acb93131a206
Jarrett Baugh <jarrett.baugh@gmail.com>
2013-10-05 19:17:47 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Estator::Application.config.secret_token = '84a4d171e6e67454edf8f72803ee6baa2d80aa11459e643640879ee34cddb33d311e97ac16e586bc8e20222f815625ffcc1ecd9e78c035ea807df99bf8000168'
