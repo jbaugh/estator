6274b634e246c96162131f84e45c92d2fb31f5c1
Jarrett Baugh <jarrett.baugh@gmail.com>
2013-10-07 21:35:47 -0700

    Fix and add some filter tests

--- spec/controllers/properties_controller_spec.rb
+++ spec/controllers/properties_controller_spec.rb
@@ -12,7 +12,7 @@ describe PropertiesController do
 
     describe "index" do
       it "should find properties" do
-        @relation.should_receive(:where)
+        @relation.should_not_receive(:where)
         get :index
       end
     end
@@ -20,7 +20,6 @@ describe PropertiesController do
     describe "rentals" do
       it "should find properties" do
         @relation.should_receive(:rentals).and_return(@relation)
-        @relation.should_receive(:where)
         get :rentals
       end
     end
@@ -28,7 +27,6 @@ describe PropertiesController do
     describe "for_sale" do
       it "should find properties" do
         @relation.should_receive(:for_sale).and_return(@relation)
-        @relation.should_receive(:where)
         get :for_sale
       end
     end
@@ -36,7 +34,6 @@ describe PropertiesController do
     describe "houses" do
       it "should find properties" do
         @relation.should_receive(:houses).and_return(@relation)
-        @relation.should_receive(:where)
         get :houses
       end
     end
@@ -44,7 +41,6 @@ describe PropertiesController do
     describe "condos" do
       it "should find properties" do
         @relation.should_receive(:condos).and_return(@relation)
-        @relation.should_receive(:where)
         get :condos
       end
     end
@@ -52,7 +48,6 @@ describe PropertiesController do
     describe "apartments" do
       it "should find properties" do
         @relation.should_receive(:apartments).and_return(@relation)
-        @relation.should_receive(:where)
         get :apartments
       end
     end
@@ -60,8 +55,63 @@ describe PropertiesController do
     describe "recent" do
       it "should find properties" do
         @relation.should_receive(:recent).and_return(@relation)
-        @relation.should_receive(:where)
         get :recent
+      end
+    end
+
+    describe "setup_search" do
+      before(:all) do
+        @value = "1000"
+      end
+
+      it "should find properties" do
+        @relation.should_receive(:more_expensive_than).with(@value)
+        get :index, :price_min => @value
+      end
+
+      it "should find properties" do
+        @relation.should_receive(:cheaper_than).with(@value)
+        get :index, :price_max => @value
+      end
+
+      it "should find properties" do
+        @relation.should_receive(:at_least_beds).with(@value)
+        get :index, :beds => @value
+      end
+
+      it "should find properties" do
+        @relation.should_receive(:at_least_baths).with(@value)
+        get :index, :baths => @value
+      end
+
+      it "should find properties" do
+        @relation.should_receive(:more_sq_ft_than).with(@value)
+        get :index, :square_feet_min => @value
+      end
+
+      it "should find properties" do
+        @relation.should_receive(:less_sq_ft_than).with(@value)
+        get :index, :square_feet_max => @value
+      end
+
+      it "should find properties" do
+        @relation.should_receive(:more_lot_size_than).with(@value)
+        get :index, :lot_size_min => @value
+      end
+
+      it "should find properties" do
+        @relation.should_receive(:less_lot_size_than).with(@value)
+        get :index, :lot_size_max => @value
+      end
+
+      it "should find properties" do
+        @relation.should_receive(:newer_than).with(@value)
+        get :index, :year_min => @value
+      end
+
+      it "should find properties" do
+        @relation.should_receive(:older_than).with(@value)
+        get :index, :year_max => @value
       end
     end
 
